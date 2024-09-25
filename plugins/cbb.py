fdf1092239dcc679fdc3a79f8ce611eb3beb01c4
iandinoo <nuriadeviana@gmail.com>
2024-09-25 11:33:14 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ CREATOR : <a href='tg://user?id={OWNER_ID}'>OWNER</a>\n○ ʙᴏᴋᴇᴘ ɢʀᴀᴛɪs : <a href='https://t.me/infobokepid'>ɪɴғᴏ ʙᴏᴋᴇᴘ</a>\n○ menerima jasa pembuatan bot chat owner</b>",
+            text = f"<b>○ ᴄʀᴇᴀᴛᴏʀ : <a href='tg://user?id={OWNER_ID}'>ɪᴀɴ ᴅɪɴᴏ</a>\n○ ʙᴏᴋᴇᴘ ɢʀᴀᴛɪs : <a href='https://t.me/infobokepid'>ɪɴғᴏ ʙᴏᴋᴇᴘ</a>\n○ menerima jasa pembuatan bot chat owner</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
